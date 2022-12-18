1c77c0395614c1a42bb2e03dfcb8bc740f90cd83
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2022-12-18 20:13:30 +0530

    !!

--- dynamic_programming/cut_rod.cpp
+++ dynamic_programming/cut_rod.cpp
@@ -22,20 +22,21 @@ int cutRod(vector<int>& rod, int n, unordered_map<int, int>& hmap) {
     return q;
 }
 
-// does'nt work
-// int altCut(vector<int>& rod, int n, int c) {
-//     if(n<0 || c==0) return 0;
-//     if(c-(n+1)>=0) {
-//         return max(rod[n] + altCut(rod, n-1, c-(n+1)), altCut(rod, n-1, c));
-//     } 
-//     return altCut(rod, n-1, c);
-// }
+//does'nt work bcoz el won't be repeated more than once
+//ie, if knapsack takes an el, it removes it from rem options to take
+int altCut(vector<int>& rod, int n, int c) {
+    if(n<0 || c==0) return 0;
+    if(c-(n+1)>=0) {
+        return max(rod[n] + altCut(rod, n-1, c-(n+1)), altCut(rod, n-1, c));
+    } 
+    return altCut(rod, n-1, c);
+}
 
 int main() {
-    vector<int> p = {1, 5, 8, 9, 10, 35, 17, 20, 24, 30};
+    vector<int> p = {3, 5, 8, 9, 10, 35, 17, 20, 24, 30};
     unordered_map<int, int>hmap;
     int x = cutRod(p, 10, hmap);
-    //int y = altCut(p, 9, 10);
+    int y = altCut(p, 9, 10);
     cout<<x<<"\n";
-    //cout<<y;
+    cout<<y;
 }
