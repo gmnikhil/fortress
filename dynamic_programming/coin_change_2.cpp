d9df02f0f3eae2f12ccb035cdbc4eb1068e7b65e
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2022-12-19 15:13:18 +0530

    deer O dear

--- dynamic_programming/coin_change_2.cpp
+++ dynamic_programming/coin_change_2.cpp
@@ -3,6 +3,8 @@
 #include<unordered_set>
 #include<string>
 
+#define INT_MAX 2147483647
+
 using namespace std;
 
 class Solution {
@@ -65,10 +67,41 @@ public:
 
         return count;
     }
+
+    int changeDP(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<int>t(amount+1, 0);
+        vector<vector<int>>dp(n+1, t);
+
+        int min = INT_MAX;
+        for(int x:coins) {
+            if(x<min) min = x;
+        }
+        if(min>amount) return 1;
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=amount; j++) {
+                if(coins[i-1] <= j) {
+                    if(coins[i-1] % j == 0) {
+                        dp[i][j] = 1 + max(dp[i][j-coins[i-1]], dp[i-1][j]);
+                    } else {
+                        dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                    }
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][amount];
+    }
 };
+
 
 int main() {
     Solution s = Solution();
     vector<int>coins={1,2,5};
     cout<<s.change(500, coins);
+    cout<<"\n";
+    cout<<s.changeDP(500, coins);
 }
