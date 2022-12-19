beed125b710f6635a0e689017d9876a52e22941e
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2022-12-19 19:31:38 +0530

    🙂🙂

--- dynamic_programming/coin_change_1.cpp
+++ dynamic_programming/coin_change_1.cpp
@@ -74,14 +74,10 @@ public:
         if(smallest>amount) return -1;
         
         for(int i=1; i<=n; i++) {
+            dp[i][0]=0;
             for(int j=1; j<=amount; j++) {
                 if(j-coins[i-1] >= 0) {
-                    if(j%coins[i-1] == 0) {
-                        dp[i][j] = min(j/coins[i-1], 1+dp[i][j-coins[i-1]]);
-                        dp[i][j] = min(dp[i][j], dp[i-1][j]);
-                    }
-                    else
-                        dp[i][j] = min(dp[i][j-coins[i-1]]+1, dp[i-1][j]);
+                    dp[i][j] = min(dp[i][j-coins[i-1]]+1, dp[i-1][j]);
                 } else {
                     dp[i][j] = dp[i-1][j];
                 }
